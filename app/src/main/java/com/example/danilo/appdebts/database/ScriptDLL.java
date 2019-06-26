c962c935dc057c2a29dd6a71abedab02d21d9c51
Adson Guyllan <adsguyll365@gmail.com>
2019-06-26 10:27:47 -0300

    debts

---
+++ app/src/main/java/com/example/danilo/appdebts/database/ScriptDLL.java
@@ -0,0 +1,28 @@
+package com.example.danilo.appdebts.database;
+
+/**
+ * Created by aluno on 26/06/19.
+ */
+
+public class ScriptDLL {
+    public static String createTableCategory(){
+        StringBuilder sql = new StringBuilder();
+        sql.append("CREATE TABLE categoria (");
+        sql.append(" id INTEGER NOT NULL PRIMARY KEY,");
+        sql.append(" tipo TEXT NOT NULL");
+        sql.append(");");
+        return sql.toString();
+        }
+
+    public static void createTableDebts() {
+        StringBuilder sql = new StringBuilder();
+        sql.append("CREATE TABLE dividas (");
+        sql.append(" id INTEGER NOT NULL PRIMARY KEY,");
+        sql.append(" cod_cat TEXT NOT NULL");
+        sql.append(" valor REAL NOT NULL,");
+        sql.append(" descricao TEXT NOT NULL,");
+        sql.append(" data_vencimento DATE NOT NULL,");
+        sql.append(" data_pagamento DATE,");
+        sql.append("FOREIGN KEY (cod_cat) REFERENCES");
+    }
+}
